26f33ac68e1ccdb0e3ef9d1535944e371c1e9f82
Andrei Popa <andreistandard@gmail.com>
2023-11-22 18:08:34 +0200

    aceasta este var corectata, te rog fa un main

--- Chromosome.cpp
+++ Chromosome.cpp
@@ -6,26 +6,26 @@
 std::random_device random_device;
 std::mt19937 random_generator(random_device());
 
-class Gene {
+class Chromosome {
 public:
-  std::vector<char> chromosomes;
+  std::vector<char> genes;
   double fitness = -1;
 
-  Gene(int number_of_chromosomes) {
+  Chromosome(int number_of_genes) {
     std::uniform_int_distribution<int> distribution(0, 1);
 
-    for (int i = 0; i < number_of_chromosomes; i++) {
+    for (int i = 0; i < number_of_genes; i++) {
       int bit = distribution(random_generator);
 
       if (bit == 0)
-        chromosomes.push_back('0');
+        genes.push_back('0');
       else
-        chromosomes.push_back('1');
+        genes.push_back('1');
     }
   }
 
-  void flip_chromosome(int index) {
-    auto &bit = chromosomes.at(index);
+  void flip_gene(int index) {
+    auto &bit = genes.at(index);
 
     if (bit == '0')
       bit = '1';
@@ -35,17 +35,17 @@ public:
 
   std::vector<double> to_numbers(MathFunction math_function, int dimensions ) {
     std::vector<double> result;
-    int chromosomes_per_dimension = chromosomes.size() / dimensions;
+    int genes_per_dimension = genes.size() / dimensions;
 
     for (int d = 0; d < dimensions; d++) {
       double sum = 0.0;
-      for (int i = 0; i < chromosomes_per_dimension; i++) {
-        sum = sum * 2 + (chromosomes[d * chromosomes_per_dimension + i] - '0');
+      for (int i = 0; i < genes_per_dimension; i++) {
+        sum = sum * 2 + (chromosomes[d * genes_per_dimension + i] - '0');
       }
 
       double real_val = math_function.bounds.min +
                         sum * (math_function.bounds.max - math_function.bounds.min) /
-                            (pow(2, chromosomes_per_dimension) - 1);
+                            (pow(2, genes_per_dimension) - 1);
       result.push_back(real_val);
     }
 
